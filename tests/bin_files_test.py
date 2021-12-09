8497600496c0eeac580b25c2ca40312b67eed0d6
ackagel <ackagel@ucdavis.edu>
2021-12-09 23:49:42 +0000

    adds to-be-filled testing framework

---
+++ tests/bin_files_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+from mibi_bin_tools import bin_files
+
+def test_mass2tof():
+    pass
+
+def test_set_tof_ranges():
+    pass
+
+def test_write_out():
+    pass
+
+def test_find_bin_files():
+    pass
+
+def test_fill_fov_metadata():
+    pass
+
+def test_parse_global_panel():
+    pass
+
+def test_parse_df_panel():
+    pass
+
+def test_parse_intensities():
+    pass
+
+def test_extract_bin_files():
+    pass
+
+def test_get_width_histogram():
+    pass
